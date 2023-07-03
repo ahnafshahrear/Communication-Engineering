904d9301c7e1e4f7f3151d168b46af330fd74099
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-03 18:54:28 +0600

    DIFFERENTIAL-MANCHESTER updated

--- DIFFERENTIAL-MANCHESTER.m
+++ DIFFERENTIAL-MANCHESTER.m
@@ -5,50 +5,51 @@ close all; #... Clear figures
 bits = [1 0 1 1 0 0 1];
 
 #... Modulation
-
-bitrate = 1; #... Number of bits per second
+bitrate = 1;
 voltage = 5;
 
-samplingRate = 1000;
-samplingTime = 1/samplingRate;
+sampling_rate = 1000;
+sampling_time = 1/sampling_rate;
 
-endTime = length(bits)/bitrate;
-time = 0:samplingTime:endTime;
+end_time = length(bits)/bitrate;
+time = 0:sampling_time:end_time;
 
 index = 1;
-sign = 1;
-if bits(1) == 0
-    sign = -1;
+#... Assume that last state was positive
+sign = 1; #... Indicates inversion
+if bits(index) == 0
+    sign = -1*sign;
 endif
 
 for i = 1:length(time)
-    if time(i)*bitrate < index-bitrate/2
+    if time(i)*bitrate < index-(bitrate/2)
         modulation(i) = sign*voltage;
     else
         modulation(i) = -sign*voltage;
     endif
     if time(i)*bitrate >= index
         index = index+1;
+        #... Inversion when next bit "0"
         if index <= length(bits) && bits(index) == 1
             sign = -1*sign;
         endif
     endif
 endfor
 
-plot(time, modulation, "LineWidth", 1);
-axis([0 endTime -voltage-5 voltage+5]);
+plot(time, modulation);
+axis([0 end_time -voltage-5 voltage+5]);
+line([0 end_time], [0,0]);
 grid on;
 
 #... Demodulation
-
 index = 1;
 last = voltage;
 
 for i = 1:length(modulation)
-    if modulation(i) == last && time(i)*bitrate < index-bitrate/2
+    if modulation(i) == last && time(i)*bitrate < index-(bitrate/2)
         demodultaion(index) = 1;
     endif
-    if modulation(i) != last && time(i)*bitrate < index-bitrate/2
+    if modulation(i) != last && time(i)*bitrate < index-(bitrate/2)
         demodultaion(index) = 0;
     endif
     if time(i)*bitrate >= index
