c8dfce2b3ff74a76ab73124ec49f8561e2931019
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-04 10:28:48 +0600

    ASK updated

--- ASK.m
+++ ASK.m
@@ -2,11 +2,10 @@ clc; #... Clear command line
 clear all; #... Clear variables
 close all; #... Clear figures
 
-bits = [1 0 1 1 0 0 1]; #... Bitstream
+bits = [1 0 1 1 0 0 1];
 
 #... Modulation
-
-bitrate = 1; #... Number of bits per second
+bitrate = 1;
 
 sampling_rate = 100;
 sampling_time = 1/sampling_rate;
@@ -18,33 +17,32 @@ a = 3; #... Amplitude
 f = 2; #... Frequency
 modulation = a*sin(2*pi*f*time);
 
-in = 1; #... Bitstream index
+index = 1;
 
 for i = 1:length(time)
-    if bits(in) == 0
+    if bits(index) == 0
         modulation(i) = 0;
     endif
-    if time(i)*bitrate >= in
-        in = in+1;
+    if time(i)*bitrate >= index
+        index = index+1;
     endif
 endfor
 
-plot(time, modulation, "LineWidth", 1);
-axis([0 end_time -10 10]);
+plot(time, modulation);
+axis([0 end_time -2*a 2*a]);
 grid on;
 
 #... Demodulation
-
-in = 1;
+index = 1;
 
 for i = 1:length(modulation)
     if modulation(i) != 0
-        demodultaion(in) = 1;
+        demodultaion(index) = 1;
     else
-        demodultaion(in) = 0;
+        demodultaion(index) = 0;
     endif
-    if time(i)*bitrate >= in
-        in = in+1;
+    if time(i)*bitrate >= index
+        index = index+1;
     endif
 endfor
 
