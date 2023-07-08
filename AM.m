04e38d956a588d80590e0a068f5289842fee4cc0
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-09 00:18:44 +0600

    README updated

--- AM.m
+++ AM.m
@@ -2,70 +2,29 @@ clc; #... Clear command line
 clear all; #... Clear variables
 close all; #... Clear figures
 
-bits = [1 0 0 0 0 0 0 0 0 1 0 1];
+msg_amplitude = 1; #... Amplitude of message signal
+msg_frequency = 2; #... Frequency(Hz) of message signal
+time = 0:0.001:3; #... Time samples
+message_signal = msg_amplitude*sin(2*pi*msg_frequency*time); #... Message signal
+subplot(4,1,1);
+plot(time, message_signal);
+title('Message Signal');
+
+carrier_amplitude = 1; #... Amplitude of carrier signal
+carrier_frequency = 30; #... Frequency(Hz) of carrier signal
+carrier_signal = carrier_amplitude*sin(2*pi*carrier_frequency*time); #... Carrier signal
+subplot(4,1,2);
+plot(time, carrier_signal);
+title('Carrier Signal');
 
 #... Modulation
-bitrate = 1;
-voltage = 5;
-
-sampling_rate = 1000;
-sampling_time = 1/sampling_rate;
-
-end_time = length(bits)/bitrate;
-time = 0:sampling_time:end_time;
-
-count = 0;
-for i = 1:length(bits)
-  if bits(i) == 0
-    count = count+1;
-  else
-    count = 0;
-  endif
-  if count == 8
-    bits(i-4) = -1;
-    bits(i-3) = -1;
-    bits(i-1) = 1;
-    bits(i) = 1;
-    count = 0;
-  endif
-endfor
-
-disp(bits);
-
-index = 1;
-sign = -1;
-
-for i = 1:length(time)
-  modulation(i) = bits(index)*sign*voltage;
-  if time(i)*bitrate >= index
-        index = index+1;
-        if index <= length(bits) && bits(index) != 0
-            sign = -sign;
-        endif
-    endif
-endfor
-
-plot(time, modulation);
-axis([0 end_time -voltage-5 voltage+5]);
-line([0 end_time], [0,0]);
-grid on;
+modulated_signal = (carrier_amplitude+message_signal).*sin(2*pi*carrier_frequency*time);
+subplot(4,1,3);
+plot(time, modulated_signal);
+title('Modulated Signal');
 
 #... Demodulation
-index = 1;
-
-for i = 1:length(modulation)
-  demodulation(index) = modulation(i)/voltage;
-  if time(i)*bitrate >= index
-    index = index+1;
-  endif
-endif
-
-for i = 1:length(demodulation)
-  if demodulation(i) == -1
-    for j = 0:7
-      demodulation(i+j) = 0;
-    endfor
-  endif
-endif
-
-disp(demodulation);
+demodulated_signal = (modulated_signal./carrier_signal)-carrier_amplitude;
+subplot(4,1,4);
+plot(time, demodulated_signal);
+title('Demodulated Signal');
